f9163104011b509f223bf4b75a73779b1f20ae20
mbisovskyi <mbisovskyi@gmail.com>
2023-01-04 18:24:58 -0500

    Created component ListReservations + Group component has functionality to pull out all the reservations belong to that group and pass it down to its child component ListReservations

--- frontend/src/Components/Group/Group.jsx
+++ frontend/src/Components/Group/Group.jsx
@@ -1,14 +1,33 @@
 //Styles
 import "./Group.css";
+//Utils
+import axios from "axios";
 //Custom Hooks
 import useAuth from "../../hooks/useAuth";
 //Components
 import GroupController from "../GroupController/GroupController";
 import AddReservation from "../AddReservation/AddReservation";
+import ListReservations from "../ListReservations/ListReservations";
+import { useState, useEffect } from "react";
 
 const Group = ({ data }) => {
   //Custom Hooks Variables
   const [user, token] = useAuth();
+  //State Variables
+  const [reservations, setReservations] = useState([]);
+
+  useEffect(() => {
+    getGroupReservations();
+  }, []);
+
+  //Sends a GET request to get all the reservations belong to its group and catches it in the state variable "reservations".
+  async function getGroupReservations() {
+    const response = await axios.get(
+      `http://127.0.0.1:8000/api/rows/${data.group.id}/`,
+      { headers: { Authorization: "Bearer " + token } }
+    );
+    setReservations(response.data);
+  }
 
   return (
     <div className="group-container">
@@ -40,6 +59,11 @@ const Group = ({ data }) => {
       </section>
       <main name="Group Data Section">
         {!user.is_owner ? <AddReservation data={data} /> : null}
+        {user.is_owner ? (
+          <ListReservations reservations={reservations} />
+        ) : (
+          <ListReservations />
+        )}
       </main>
       <footer name="Group Footer">Footer</footer>
     </div>
---
+++ frontend/src/Components/ListReservations/ListReservations.jsx
@@ -0,0 +1,8 @@
+//Styling
+import "./ListReservations.css";
+
+const ListReservations = ({ reservations }) => {
+  return <div className="listreservations-container"></div>;
+};
+
+export default ListReservations;
